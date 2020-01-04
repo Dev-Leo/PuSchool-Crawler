14ac45edce02e838079da637216df57074651409
Dev-Leo <33715512+Dev-Leo@users.noreply.github.com>
2020-01-04 08:00:00 +0000

    Update pu.py

--- pu.py
+++ pu.py
@@ -6,7 +6,7 @@ headers = {
 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
 'Connection': 'keep-alive',
 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-'Cookie': 'PHPSESSID=7b3404645d55384acdc47; route=346a37577cefae76d85037d398f41fa7; Hm_lvt_dd3ea352543392a029ccf9da1be54a50=1565866060; Hm_lpvt_dd3ea352543392a029ccf9da1be54a50=1565880271; TS_LOGGED_USER=Gh3AMyGxDyG9R8Hoo70HRSEws'
+'Cookie': 'You cookie'
 }
 
 urllist = []
